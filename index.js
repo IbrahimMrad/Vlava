0475a8e33774e11759e8d4474abe013734ae7248
admin <ibmrad60@gmail.com>
2024-05-24 10:45:45 +0300

    Linking web template to my API - second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 fetch('https://vlavaapi-4da1.onrender.com/services')
     .then(response => response.json())
     .then(services => {
-        var services_row = document.getElementById("services_row");
+        var services_row = document.getElementById('services_row');
         services.forEach(service => {
             var _div = document.createElement('div');
             var _current_content = `<div class="icon"><i class="bi bi-briefcase"></i></div>
@@ -12,3 +12,25 @@ fetch('https://vlavaapi-4da1.onrender.com/services')
             services_row.appendChild(_div);
         });
     })
+
+var _happy_clients = [
+    { name: 'Ahmad', image: 'gggggggghhhhh' },
+    { name: 'Sandra', image: 'gggggggghhhhh' }
+];
+
+const happy_clients_div = document.getElementById('happy_clients_div');
+_happy_clients.forEach(client => {
+    var _div = document.createElement('div');
+    _div.className = 'swiper-slide';
+    _div.innerHTML = `<div class="testimonial-item">
+            <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
+            <h3>${client.name}</h3>
+            <h4>Ceo &amp; Founder</h4>
+            <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+            </p>
+        </div>`;
+    happy_clients_div.appendChild(_div);
+});
